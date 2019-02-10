6fb2f98187cca646fe5a830eeb2ed63226a37917
Clay Anderson <claya@uber.com>
2019-02-09 21:15:16 -0700

    WIP: Initial mockup

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import ShoppingList from './views/ShoppingList.vue';
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/shopping-list',
+      name: 'shopping-list',
+      component: ShoppingList,
     },
     {
       path: '/about',
---
+++ src/views/ShoppingList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="shopping-list">
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Item</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><input type="checkbox" /></td>
+          <td>Milk</td>
+        </tr>
+        <tr>
+          <td><input type="checkbox" /></td>
+          <td>Eggs</td>
+        </tr>
+        <tr>
+          <td><input type="checkbox" /></td>
+          <td>Cereal</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class ShoppingList extends Vue {}
+</script>
