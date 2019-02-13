63a6fc6f1a2d783993490d37dc3c363267ab837a
Clay Anderson <claya@uber.com>
2019-02-12 21:03:27 -0700

    List, complete, and uncomplete functionality

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 import ShoppingList from './views/ShoppingList.vue';
 
 Vue.use(Router);
@@ -12,7 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: ShoppingList,
     },
     {
       path: '/shopping-list',
@@ -20,12 +19,12 @@ export default new Router({
       component: ShoppingList,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/recipes',
+      name: 'recipes',
       // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
+      // this generates a separate chunk (recipe-list.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import(/* webpackChunkName: "recipe-list" */ './views/RecipeList.vue'),
     },
   ],
 });
--- src/views/ShoppingList.vue
+++ src/views/ShoppingList.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="shopping-list">
-    <table>
+
+    <h3>Items</h3>
+    <div v-for="item of items" :key="item.uuid" @click="completeItem(item.uuid)">
+      <!-- <td><input type="checkbox" /></td> -->
+      {{item.name}}
+    </div>
+
+    <h3 v-if="completedItems.length">Completed</h3>
+    <div v-for="item of completedItems" :key="item.uuid" @click="uncompleteItem(item.uuid)">
+      <!-- <td><input type="checkbox" /></td> -->
+      <span style="color:red">{{item.name}}</span>
+    </div>
+
+    <!-- <table>
       <thead>
         <tr>
           <th></th>
@@ -8,7 +21,15 @@
         </tr>
       </thead>
       <tbody>
+        <tr v-for="item of items" :key="item.uuid" @click="completeItem(item.uuid)">
+          <td>{{item.name}}</td>
+        </tr>
         <tr>
+          <td>
+        <tr v-for="item of completedItems" :key="item.uuid" @click="uncompleteItem(item.uuid)"
+          <td>{{item.name}}</td>
+        </tr> -->
+        <!-- <tr>
           <td><input type="checkbox" /></td>
           <td>Milk</td>
         </tr>
@@ -19,18 +40,84 @@
         <tr>
           <td><input type="checkbox" /></td>
           <td>Cereal</td>
-        </tr>
-      </tbody>
-    </table>
+        </tr> -->
+      <!-- </tbody>
+    </table> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
+type GroceryItem = { uuid: string, name: string };
+
+const INITIAL_LIST: GroceryItem[] = [
+  {
+    uuid: '1',
+    name: 'milk'
+  },
+  {
+    uuid: '2',
+    name: 'eggs'
+  },
+  {
+    uuid: '3',
+    name: 'cereal'
+  },
+  {
+    uuid: '4',
+    name: 'butter'
+  },
+];
+
+
 @Component({
   components: {
   },
 })
-export default class ShoppingList extends Vue {}
+export default class ShoppingList extends Vue {
+  items: GroceryItem[]  = INITIAL_LIST;
+  completedItems: GroceryItem[]  = [];
+
+  addItem({name}: {name: string}) {
+    const newItem = {
+      uuid: 'todo',
+      name,
+    };
+    this.items = [...this.items, newItem];
+  }
+
+  completeItem(uuid: string) {
+    const index = this.items.findIndex(i => i.uuid === uuid);
+    if (index === -1) {
+      throw Error(`UUID not found: ${uuid}`);
+    }
+    const item = this.items[index];
+
+    this.items = [
+      ...this.items.slice(0, index),
+      ...this.items.slice(index + 1),
+    ];
+
+    this.completedItems = [item, ...this.completedItems];
+  }
+
+  uncompleteItem(uuid: string) {
+    const index = this.completedItems.findIndex(i => i.uuid === uuid);
+    if (index === -1) {
+      throw Error(`UUID not found: ${uuid}`);
+    }
+    const item = this.completedItems[index];
+
+    this.items = [
+      ...this.items,
+      item,
+    ];
+
+    this.completedItems = [
+      ...this.completedItems.slice(0, index),
+      ...this.completedItems.slice(index + 1),
+    ];
+  }
+}
 </script>
